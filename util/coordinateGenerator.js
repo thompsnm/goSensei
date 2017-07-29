07d66082801e3d5e06b66e6beb1e08810de2576a
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2017-07-29 16:29:38 -0600

    Add utility to generate coordinate list

--- util/coordinateGenerator.js
+++ util/coordinateGenerator.js
@@ -2,6 +2,6 @@ var columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
 
 columns.forEach(function(column) {
   for(var i = 1; i <= columns.length; i++) {
-    console.log(column + i);
+    console.log(column + ' ' + i);
   }
 });
