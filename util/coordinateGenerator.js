6ec489a26393284206e7805aac3bd6bb275dbcfa
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2017-07-29 14:25:17 -0600

    Create util for generating coordinate custom type

---
+++ util/coordinateGenerator.js
@@ -0,0 +1,7 @@
+var columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+
+columns.forEach(function(column) {
+  for(var i = 1; i <= columns.length; i++) {
+    console.log(column + i);
+  }
+});
