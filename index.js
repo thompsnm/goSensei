bc8eaabba41fdfc1cdfef01368c03c44723fd11f
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2017-07-29 12:30:05 -0600

    Wrap gnugo executor in Node

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
+var exec    = require('child_process').exec;
+var fs      = require('fs');
 var program = require('commander');
-var exec = require('child_process').exec;
+var sgf2go  = require('sgf2go');
 
 program.version('1.0.0');
 
-console.log(__dirname);
-const gnugo = exec(__dirname + '/gnugo -h');
-//const gnugo = exec('ls ' + __dirname);
+var json = [
+    [
+      [
+        {"key":"FF","value":["4"]},
+        {"key":"GM","value":["1"]},
+        {"key":"SZ","value":["9"]}
+      ],
+      [
+        {"key":"B","value":["aa"]}
+      ],
+      [
+        {"key":"W","value":["bb"]}
+      ],
+      [
+        {"key":"B","value":["cc"]}
+      ],
+      [
+        {"key":"W","value":["dd"]}
+      ],
+      [
+        {"key":"B","value":["ad"]}
+      ],
+      [
+        {"key":"W","value":["bd"]}
+      ]
+    ]
+];
 
-gnugo.stdout.on('data', (data) => {
-    console.log(data.toString());
-});
+generateInitialSgf();
+executeMove();
 
-gnugo.stderr.on('data', (data) => {
-    console.log(data.toString());
-});
+// Generate initial sgf file
+function generateInitialSgf() {
+  const generated = sgf2go.json2sgf(json);
+  //const generated = fs.readFileSync(`${__dirname}/game.sgf`).toString();
+  fs.writeFileSync(__dirname + '/game.sgf', generated);
+  console.log('Current sgf:');
+  logSgfFile();
+}
 
-gnugo.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});
+//Execute move
+function executeMove() {
+  const gnugo = exec(`${__dirname}/gnugo -l ${__dirname}/game.sgf -o ${__dirname}/game.sgf`);
+
+  gnugo.stdout.on('data', (data) => {
+    console.log(data);
+  });
+
+  gnugo.stderr.on('data', (data) => {
+    console.log(data);
+  });
+
+  gnugo.on('close', (code) => {
+    console.log('New sgf:');
+    logSgfFile();
+  });
+};
+
+function logSgfFile() {
+  console.log(fs.readFileSync(`${__dirname}/game.sgf`).toString());
+}
